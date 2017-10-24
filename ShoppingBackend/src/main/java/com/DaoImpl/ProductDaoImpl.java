41920f07e1f79a7a71a55ebca3240244765b0689
Aishwarya Arora <aishwarya23arora@gmail.com>
2017-10-25 01:26:03 +0530

    Project Updated

--- ShoppingBackend/src/main/java/com/DaoImpl/ProductDaoImpl.java
+++ ShoppingBackend/src/main/java/com/DaoImpl/ProductDaoImpl.java
@@ -2,6 +2,8 @@ package com.DaoImpl;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -47,17 +49,37 @@ public class ProductDaoImpl implements ProductDao
 
 	public List<Product> retrieveProducts() {
 		// TODO Auto-generated method stub
-		return null;
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from Product");
+		List<Product> listProducts=query.list();
+		session.close();
+		return listProducts;
 	}
 
 	public boolean updateProduct(Product product) {
-		// TODO Auto-generated method stub
+		
 		return false;
+		// TODO Auto-generated method stub
+		/*try
+		{
+		sessionFactory.getCurrentSession().saveOrUpdate(product);
+		return true;
+		}
+		catch(Exception e)
+		{
+		System.out.println("Exception Arised:"+e);
+		return false;
+		}*/
+		
 	}
 
 	public Product getProduct(int productId) {
 		// TODO Auto-generated method stub
-		return null;
+		Session session=sessionFactory.openSession();
+		Product product=(Product)session.get(Product.class, productId);
+		session.close();
+		return product;
+		
 	}
 
 }
